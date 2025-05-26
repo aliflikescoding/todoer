90c513d692324546c79cf57e5ab922882885f912
Alif Wibowo <alif.likes.coding@gmail.com>
2025-05-27 00:39:48 +0700

    Remove primary and seconedar

--- demo/src/main/java/com/example/AllController.java
+++ demo/src/main/java/com/example/AllController.java
@@ -4,17 +4,6 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class AllController {
-
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
-
-    @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
-    }
-
     @FXML
     private void switchToHome() throws IOException {
         App.setRoot("home");
