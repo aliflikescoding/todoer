b095a2aa6cf8e6e13fd2efda8303fa93802e9a30
adrianobwn <167875657+adrianobwn@users.noreply.github.com>
2025-05-27 00:43:14 +0700

    ini update

--- demo/src/main/java/com/logic/Shopping.java
+++ demo/src/main/java/com/logic/Shopping.java
@@ -2,11 +2,11 @@ package com.logic;
 
 public class Shopping extends Task {
   public Shopping(String name, String description, boolean done) {
-        super(name, description, done);
-    }
+      super(name, description, done);
+  }
 
-    @Override
-    public void method() {
-        System.out.println("Executing Shopping Task: " + name);
-    }
+  @Override
+  public void method() {
+    System.out.println("Executing Shopping Task: " + name);
+  }
 }
