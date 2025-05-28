4d6d8b0d7f012e020b203f2d1f672ffa1707c6f3
Alif Wibowo <alif.likes.coding@gmail.com>
2025-05-29 01:58:34 +0700

    Add work task stuff

---
+++ demo/src/main/java/com/example/CreateWorkController.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
+public class CreateWorkController {
+  @FXML
+    private void switchToHome() throws IOException {
+        App.setRoot("home");
+    }
+
+    @FXML
+    private void switchToPersonal() throws IOException {
+        App.setRoot("personal");
+    }
+
+    @FXML
+    private void switchToWork() throws IOException {
+        App.setRoot("work");
+    }
+
+    @FXML
+    private void switchToShopping() throws IOException {
+        App.setRoot("shopping");
+    }
+}
---
+++ demo/src/main/java/com/example/WorkController.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
+public class WorkController {
+  @FXML
+  private void switchToHome() throws IOException {
+      App.setRoot("home");
+  }
+
+  @FXML
+  private void switchToPersonal() throws IOException {
+      App.setRoot("personal");
+  }
+
+  @FXML
+  private void switchToWork() throws IOException {
+      App.setRoot("work");
+  }
+
+  @FXML
+  private void switchToShopping() throws IOException {
+      App.setRoot("shopping");
+  }
+
+  @FXML
+  private void switchToCreateWork() throws IOException {
+    App.setRoot("createWork");
+  }
+}
