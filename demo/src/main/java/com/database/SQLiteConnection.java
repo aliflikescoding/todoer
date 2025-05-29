6368073079a0d2d20dc687dda46b1a67684db77d
Alif Wibowo <alif.likes.coding@gmail.com>
2025-05-29 21:24:22 +0700

    database mutiple

--- demo/src/main/java/com/database/SQLiteConnection.java
+++ demo/src/main/java/com/database/SQLiteConnection.java
@@ -3,15 +3,32 @@ package com.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.io.File;
-import java.net.URISyntaxException;
 
 public class SQLiteConnection {
   public static Connection getConnection() {
     try {
-      // For development, use a fixed path or user home directory
-      String dbPath = System.getProperty("user.home") + File.separator + "todoer.db";
+      String os = System.getProperty("os.name").toLowerCase();
+      String dbDir;
+
+      // Determine OS-specific data directory
+      if (os.contains("win")) {
+        dbDir = System.getenv("APPDATA") + File.separator + "Todoer";
+      } else if (os.contains("mac")) {
+        dbDir = System.getProperty("user.home") + "/Library/Application Support/Todoer";
+      } else { // Linux/Unix
+        dbDir = System.getProperty("user.home") + "/.todoer";
+      }
+
+      // Create directory if it doesn't exist
+      File dir = new File(dbDir);
+      if (!dir.exists()) {
+        dir.mkdirs();
+      }
+
+      String dbPath = dbDir + File.separator + "todoer.db";
       String url = "jdbc:sqlite:" + dbPath;
-      System.out.println("Database path: " + url); // Debug output
+
+      System.out.println("Database path: " + url); // Debug
       return DriverManager.getConnection(url);
     } catch (Exception e) {
       e.printStackTrace();
